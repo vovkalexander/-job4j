b89da5ca62fcd8743584bfa35d82f6bca9e0ff8a
vovkalexander <vovk.ag747@gmail.com>
2019-01-15 23:52:09 +0300

    1.3. JavaDoc. Code convention[#104450]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,18 @@
+package ru.job4j.calculate;
+/**
+* Calculate.
+* @author vovk  vovk.ag747@gmail.com
+* @since 15.01.2019
+* @version 1
+*/
+
+ public class Calculate{
+/**
+* Main.
+* @param args - args.
+*/
+
+  public static void main(String[] args) {
+  System.out.println("Hello world");
+}
+}
