5d1b3193b52f61be2a81044b83e6dff73169f120
vovkalexander <vovk.ag747@gmail.com>
2019-01-18 01:10:57 +0300

    Text

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -5,23 +5,23 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Vovk Alexander (vovk.ag747@gmail.com)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Vovk Alexander (vovk.ag747@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/
-@Test
-public void whenTakeNameThenThreeEchoPlusName() {
-    String input = "Vovk Alexander";
-    String expect = "Echo, echo, echo : Vovk Alexander";
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenThreeEchoPlusName() {
+        String input = "Vovk Alexander";
+        String expect = "Echo, echo, echo : Vovk Alexander";
+        Calculate calc = new Calculate();
+            String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
 
 }
