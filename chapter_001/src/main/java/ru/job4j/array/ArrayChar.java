9126d8450fe51f7f2d1ca6522be7c585768ae73d
vovkalexander <vovk.ag747@gmail.com>
2019-01-31 01:39:10 +0300

    6.4. Слово начинается с ...[#104430]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,36 @@
+package ru.job4j.array;
+/**
+ * Array.
+ * @author Vovk Alexander  vovk.ag747@gmail.com
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class ArrayChar  {
+    /**
+     * Contains data(array of char).
+     */
+    private char[] data;
+    /**
+     * Constructor ArrayChar.
+     * @param line - string variable.
+     */
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+    /**
+     * startWith .
+     * @param prefix-string variable.
+     * return boolean.
+     */
+    public boolean startWith(String prefix) {
+        boolean result = true;
+        char[] value = prefix.toCharArray();
+        for (int i = 0; i != value.length; i++) {
+            if (this.data[i] != value[i]) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
