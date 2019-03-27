4735548c51e1333e5eef5ca5b3e293ec71447f0f
vovkalexander <vovk.ag747@gmail.com>
2019-03-28 00:51:22 +0300

    1. Телефонный справочник на базе ArrayList[#104473]

---
+++ chapter_003/src/main/java/ru/job4j/search/Person.java
@@ -0,0 +1,66 @@
+package ru.job4j.search;
+/**
+ * Person.
+ * @author Vovk Alexander  vovk.ag747@gmail.com
+ * @version $Id$
+ * @since 0.1
+ */
+public class Person {
+    /**
+     * Хранит имя пользователя.
+     */
+    private String name;
+    /**
+     * Хранит фамилию пользователя.
+     */
+    private String surname;
+    /**
+     * Хранит номер телефона пользователя.
+     */
+    private String phone;
+    /**
+     * Хранит адрес пользователя.
+     */
+    private String address;
+    /**
+     * Конструктор для активации полей.
+     * @param name имя пользователя.
+     * @param surname фамилия пользователя.
+     * @param phone номер телефона пользователя.
+     * @param address адрес пользователя.
+     */
+    public Person(String name, String surname, String phone, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.address = address;
+    }
+    /**
+     * Метод возвращает имя пользователя.
+     * @return имя пользователя.
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * Метод возвращает фамилия пользователя.
+     * @return фамилия пользователя.
+     */
+    public String getSurname() {
+        return surname;
+    }
+    /**
+     * Метод возвращает номер телефона пользователя.
+     * @return номер телефона пользователя.
+     */
+    public String getPhone() {
+        return phone;
+    }
+    /**
+     * Метод возвращает адрес пользователя.
+     * @return адрес пользователя.
+     */
+    public String getAddress() {
+        return address;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.search;
+import org.junit.Test;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Test.
+ * @author Vovk Alexander (vovk.ag747@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Alexander","Vovk", "115142","Moscow"));
+        List<Person> persons = phones.find("Ale");
+        assertThat(persons.iterator().next().getSurname(), is("Vovk"));
+    }
+    @Test
+    public void whenFindByPhone() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Alexander","Vovk", "115142","Moscow"));
+        List<Person> persons = phones.find("115");
+        assertThat(persons.iterator().next().getAddress(), is("Moscow"));
+    }
+}
