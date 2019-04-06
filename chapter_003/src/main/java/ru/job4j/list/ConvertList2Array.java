5d38be889cf48009294324b0fb63cc95f4c7900c
vovkalexander <vovk.ag747@gmail.com>
2019-04-06 22:56:53 +0300

    1. Конвертация листа массивов в один лист Integer[#104479]

--- chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -1,5 +1,7 @@
 package ru.job4j.list;
+import java.util.ArrayList;
 import java.util.List;
+
 /**
  * ConvertList2Array.
  * @author Vovk Alexander  vovk.ag747@gmail.com
@@ -15,17 +17,31 @@ public class ConvertList2Array {
      */
     public int[][] toArray(List<Integer> list, int rows) {
         int cells  = (int) Math.ceil((double) list.size() / rows);
-        int[][] array= new int[rows][cells ];
+        int[][] array = new int[rows][cells ];
         int i = 0; int j = 0;
         for (Integer value : list) {
             array[i][j] = value;
             j++;
-            if (j > cells - 1 ) {
+            if (j > cells - 1) {
                 i++;
                 j = 0;
             }
         }
         return array;
+    }
+    /**
+     * Метод преобразует список в массив.
+     * @param list - список, хранит множество типа целочисленного массива.
+     * @return catalogue - список, хранит множество типа Integer.
+     */
+    public List<Integer> convert(List<int[]> list) {
+        List<Integer> catalogue = new ArrayList<>();
+        for (int[] ints : list) {
+            for (int i : ints) {
+                catalogue.add(i);
+            }
+        }
+        return catalogue;
     }
 }
 
--- chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
@@ -1,26 +1,33 @@
 package ru.job4j.list;
 import org.junit.Test;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
-
+/**
+ * Test.
+ * @author Vovk Alexander (vovk.ag747@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class ConvertList2ArrayTest {
     @Test
     public void when7ElementsThen9() {
         ConvertList2Array list = new ConvertList2Array();
-        int[][] result = list.toArray(Arrays.asList(1, 2, 3, 4, 5, 6, 7),3 );
+        int[][] result = list.toArray(Arrays.asList(1, 2, 3, 4, 5, 6, 7), 3);
         int[][] expect = {
                 {1, 2, 3},
                 {4, 5, 6},
-                {7, 0 ,0}
+                {7, 0, 0}
         };
         assertThat(result, is(expect));
     }
     @Test
     public void when9ElementsThen10() {
         ConvertList2Array list = new ConvertList2Array();
-        int[][] result = list.toArray(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9),5 );
+        int[][] result = list.toArray(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9), 5);
         int[][] expect = {
                 {1, 2},
                 {3, 4},
@@ -28,6 +35,16 @@ public class ConvertList2ArrayTest {
                 {7, 8},
                 {9, 0},
         };
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenConvert6ElementsThenHave6SequentialElements() {
+        ConvertList2Array convertList = new ConvertList2Array();
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[]{1, 2});
+        list.add(new int[]{3, 4, 5, 6});
+        List<Integer> result = convertList.convert(list);
+        List<Integer> expect = Arrays.asList(1, 2, 3, 4, 5, 6);
         assertThat(result, is(expect));
     }
 }
