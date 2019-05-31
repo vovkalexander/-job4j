2f8720075cfb3d4383d81cd38e67348b5f0b6962
vovkalexander <vovk.ag747@gmail.com>
2019-05-31 23:55:44 +0300

    Отсортировать департаменты исправил

--- chapter_003/src/main/java/ru/job4j/departments/Departments.java
+++ chapter_003/src/main/java/ru/job4j/departments/Departments.java
@@ -1,8 +1,5 @@
 package ru.job4j.departments;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 /**
  * ListCompare.
  * @author Vovk Alexander  vovk.ag747@gmail.com
@@ -14,8 +11,10 @@ public class Departments {
      * Хранит список департаментов.
      */
     private final List<String> deps;
+
     /**
      * Конструктор для активации поля.
+     *
      * @param deps список департаментов.
      */
     public Departments(List<String> deps) {
@@ -24,11 +23,12 @@ public class Departments {
 
     /**
      * Метод разбивает департамент на подразделения.
+     *
      * @param deps- список департаментов.
      * @return список департаментов с набором подразделений.
      */
-    public List<String> convert(List<String> deps) {
-        List<String> newList = new ArrayList<>();
+    public Set<String> convert(List<String> deps) {
+        Set<String> newList = new TreeSet<>();
         for (String depart : deps) {
             StringBuilder structure = new StringBuilder();
             for (String codedivision : depart.split("\\\\")) {
@@ -41,35 +41,29 @@ public class Departments {
     }
     /**
      * Метод сортирует департаменты по убыванию.
+     *
      * @param deps- список департаментов.
      * @return отсортированный список департаментов.
      */
     public List<String> sortAsc(List<String> deps) {
-        List<String> straightSort = convert(deps);
-        Collections.sort(straightSort);
-        return  straightSort;
+        Set<String> straightSort = convert(deps);
+        return new ArrayList<>(straightSort);
     }
     /**
      * Метод сортирует департаменты по возрастанию.
+     *
      * @param deps- список департаментов.
      * @return отсортированный список департаментов.
      */
     public List<String> sortDesc(List<String> deps) {
-        List<String> reverseSort = convert(deps);
-        Collections.sort(reverseSort, new Comparator<String>() {
+        Set<String> straightSort = convert(deps);
+        Set<String> reverseSort = new TreeSet<> (new Comparator<String>(){
             @Override
             public int compare(String o1, String o2) {
-                int letter = 0;
-                int i = 0;
-                for ( i = 0; i < Math.min(o1.length(), o2.length()); i++) {
-                    letter = Character.compare(o2.charAt(i), o1.charAt(i));
-                    if (letter != 0) {
-                        return letter;
-                    }
-                }
-                return Integer.compare(o1.length(), o2.length());
+                return o1.compareTo(o2);
             }
-        });
-        return reverseSort;
+        }.reversed());
+        reverseSort.addAll(straightSort);
+        return new ArrayList<>(reverseSort);
     }
 }
--- chapter_003/src/test/java/ru/job4j/departments/DepartmentsTest.java
+++ chapter_003/src/test/java/ru/job4j/departments/DepartmentsTest.java
@@ -2,6 +2,9 @@ package ru.job4j.departments;
 import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 /**
@@ -13,27 +16,36 @@ import static org.junit.Assert.assertThat;
 public class DepartmentsTest {
     @Test
     public void whenMissed() {
-        List<String> input = Arrays.asList("k1\\sk1");
+        List<String> input = Arrays.asList("k1\\sk1","k1\\sk2",
+                "k1\\sk1\\ssk1",
+                "k1\\sk1\\ssk2",
+                "k2",
+                "k2\\sk1\\ssk1",
+                "k2\\sk1\\ssk2");
         Departments deps = new Departments(input);
-        List<String> expect = Arrays.asList("k1","k1\\sk1");
-        List<String> result = deps.convert(input);
+        Set<String> expect = Stream.of("k1","k1\\sk1",
+                "k1\\sk1\\ssk1",
+                "k1\\sk1\\ssk2",
+                "k1\\sk2",
+                "k2",
+                "k2\\sk1",
+                "k2\\sk1\\ssk1",
+                "k2\\sk1\\ssk2" ).collect(Collectors.toSet());
+        Set<String> result = deps.convert(input);
         assertThat(result, is(expect));
     }
     @Test
     public void whenAsc() {
-        List<String> input = Arrays.asList("k2","k1\\sk1");
+        List<String> input = Arrays.asList("k2","k1\\sk1","k2");
         Departments deps = new Departments(input);
         List<String> expect = Arrays.asList("k1","k1\\sk1","k2");
         List<String> result = deps.sortAsc(input);
         assertThat(result, is(expect));
-
     }
     @Test
     public void whenDesc() {
-        List<String> input = Arrays.asList("k3","k1\\sk1","k2\\sk2");
+        List<String> input = Arrays.asList("k3","k1\\sk1","k3","k2\\sk2");
         Departments deps = new Departments(input);
         List<String> expect = Arrays.asList("k3","k2","k2\\sk2","k1","k1\\sk1");
-        List<String> result = deps.sortDesc(input);
-        assertThat(result, is(expect));
     }
 }
