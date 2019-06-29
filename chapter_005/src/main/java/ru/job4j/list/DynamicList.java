936310e0e56465623eb1cff7ddac4c87b036970c
vovkalexander <vovk.ag747@gmail.com>
2019-06-29 14:08:57 +0300

    5.3.1. Создать динамический список на базе массива.

---
+++ chapter_005/src/main/java/ru/job4j/list/DynamicList.java
@@ -0,0 +1,86 @@
+package ru.job4j.list;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+/**
+ * DynamicList.
+ * @author Vovk Alexander  vovk.ag747@gmail.com
+ * @version $Id$
+ * @since 0.1
+ */
+public class DynamicList<E> implements Iterable<E> {
+    /**
+     * Поле - хранит массив.
+     */
+    private Object[] container;
+    /**
+     * Поле - хранит счетчик индексов.
+     */
+    private int index = 0;
+    /**
+     * Поле - хранит размер массива.
+     */
+    private int size;
+    /**
+     * Поле - хранит счетчик изменение структуры массива.
+     */
+    private int modCount;
+    /**
+     * Конструктор для активации размера массива.
+     * @param size  размер массива.
+     */
+    public DynamicList(int size) {
+        this.size = size;
+        this.container = new Object[size];
+    }
+    /**
+     * Метод - добавляет элемент в массив.
+     * @param  value значение.
+     */
+    public void add(E value) {
+        if (index + 1 == size) {
+            size *= 2;
+            E[] newContainer = (E[])  new Object[size];
+            for (int i = 0; i < index; i++) {
+                newContainer[i] = (E) container[i];
+                container = newContainer;
+            }
+        }
+        container[index++] = value;
+        modCount++;
+    }
+    /**
+     * Метод - возвращает элемент  массива.
+     * @param index позиция массива.
+     */
+    public E get(int index) {
+        return (E) container[index];
+    }
+    /**
+     * Метод - возвращает массив.
+     */
+    public Object[] get() {
+        return this.container;
+    }
+    /**
+     * Метод - возвращает итератор .
+     */
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            private int position;
+            int expectedModCount = modCount;
+            @Override
+            public boolean hasNext() {
+                if (this.expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return position < index;
+            }
+            @Override
+            public E next() {
+                return (E) container[position++];
+            }
+        };
+    }
+}
+
+
---
+++ chapter_005/src/test/java/ru/job4j/list/DynamicListTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.list;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+/**
+ * DynamicListTest.
+ * @author Vovk Alexander  vovk.ag747@gmail.com
+ * @version $Id$
+ * @since 0.1
+ */
+public class DynamicListTest {
+    DynamicList<Integer> list;
+    @Before
+    public void setUp() {
+        list = new DynamicList<>(2);
+    }
+    @Test
+    public void whenSizeOfArrayFullThenThisSizeRisesDoubly() {
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        assertThat(list.get().length, is(4));
+    }
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenIteratorCreatedAndListChangesThenException() {
+        list.add(3);
+        list.add(4);
+        Iterator<Integer> it = list.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        list.add(5);
+        it.hasNext();
+    }
+}
