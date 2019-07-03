2fa3f47172a65ec9b7a46a30e0a2849e0ceb5d72
vovkalexander <vovk.ag747@gmail.com>
2019-07-04 02:50:44 +0300

    5.3.2. Создать контейнер на базе связанного списка.

---
+++ chapter_005/src/main/java/ru/job4j/list/DynamicLinkedList.java
@@ -0,0 +1,120 @@
+package ru.job4j.list;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+/**
+ * DynamicLinkedList.
+ * @author Vovk Alexander  vovk.ag747@gmail.com
+ * @version $Id$
+ * @since 0.1
+ */
+public class DynamicLinkedList<E> implements Iterable<E> {
+    /**
+     * Поле - хранит размер массива.
+     */
+    private int size;
+    /**
+     * Поле - хранит счетчик изменение структуры массива.
+     */
+    int modCount;
+    /**
+     * Поле - хранит ссылку на  объект внутреннего класса.
+     */
+    private Node<E> first;
+    /**
+     * Поле - хранит ссылку на  объект внутреннего класса.
+     */
+    private Node<E> last;
+    /**
+     * Метод вставляет в конец списка данные.
+     * @param data - данные.
+     */
+    public void add(E data) {
+        Node<E> newLink = new Node<>(data);
+        if (first == null) {
+            first = newLink;
+        } else {
+            last.next = newLink;
+            newLink.previous = last;
+        }
+        last = newLink;
+        this.size++;
+        modCount++;
+    }
+    /**
+     * Метод получения элемента по индексу.
+     * @param index - индекс элеммента.
+     */
+    public E get(int index) {
+        Node<E> result = this.last;
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index < size && index >= 0) {
+            for (int i = size - 1; i > index; i--) {
+                result = result.previous;
+            }
+        }
+        return result.data;
+    }
+    /**
+     * Метод получения размера коллекции.
+     */
+    public int getSize() {
+        return this.size;
+    }
+    /**
+     * Node<E> Класс предназначен для хранения данных.
+     */
+    private static class Node<E> {
+        /**
+         * Поле - хранит данные.
+         */
+        E data;
+        /**
+         * Поле - хранит ссылку на  объект следующего объкта.
+         */
+        Node<E> next;
+        /**
+         * Поле - хранит ссылку на  объект предыдущего объекта.
+         */
+        Node<E> previous;
+
+        /**
+         * Конструктор для активации поля .
+         * @param data - данные.
+         */
+        Node(E data) {
+            this.data = data;
+        }
+    }
+    /**
+     * Метод - возвращает итератор .
+     */
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            int expectedModCount = modCount;
+            Node<E> current = first;
+            @Override
+            public boolean hasNext() {
+                if (this.expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return current != null;
+            }
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E value = current.data;
+                current = current.next;
+                return value;
+            }
+        };
+    }
+}
+
+
+
+
---
+++ chapter_005/src/test/java/ru/job4j/list/DynamicLinkedListTest.java
@@ -0,0 +1,60 @@
+package ru.job4j.list;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+/**
+ * DynamicLinkedListTest.
+ * @author Vovk Alexander  vovk.ag747@gmail.com
+ * @version $Id$
+ * @since 0.1
+ */
+public class DynamicLinkedListTest {
+    @Before
+    public void setUp() {
+        DynamicLinkedList list = new DynamicLinkedList();
+    }
+    @Test
+    public void whenAddElementThenElementAddsAtEndList() {
+        DynamicLinkedList list = new DynamicLinkedList();
+        list.add(3);
+        list.add(4);
+        assertThat(list.get(1), is(4));
+    }
+    @Test
+    public void whenIterateThenListReturnsFinalValue() {
+        DynamicLinkedList list = new DynamicLinkedList();
+        list.add(3);
+        list.add(4);
+        Iterator<Integer> it = list.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(false));
+    }
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenIteratorCreatedAndListChangesThenException() {
+        DynamicLinkedList list = new DynamicLinkedList();
+        list.add(3);
+        list.add(4);
+        Iterator<Integer> it = list.iterator();
+        assertThat(it.next(), is(3));
+        list.add(5);
+        assertThat(it.next(), is(5) );
+    }
+    @Test(expected = NoSuchElementException.class)
+    public void whenIterateAfterReturningFinalValueNextThenExсeption() {
+        DynamicLinkedList list = new DynamicLinkedList();
+        list.add(3);
+        list.add(4);
+        Iterator<Integer> it = list.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        it.next();
+    }
+
+}
