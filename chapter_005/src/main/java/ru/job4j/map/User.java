2022cebf342c07b964a073e04af4b3c00b7f8e5f
vovkalexander <vovk.ag747@gmail.com>
2019-07-12 22:33:03 +0300

    1. Создать модель User[#148410]

---
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,55 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+/**
+ * User.
+ * @author Vovk Alexander  vovk.ag747@gmail.com
+ * @version $Id$
+ * @since 0.1
+ */
+public class User {
+    /**
+     * Поле - хранит имя пользователя.
+     */
+    private String name;
+    /**
+     * Поле - хранит кол-во детей у пользователя.
+     */
+    private int children;
+    /**
+     * Поле - хранит дату рождения пользователя.
+     */
+    private Calendar birthday;
+    /**
+     * Конструктор для активации полей пользователя.
+     * @param name имя пользователя.
+     * @param children количество детей пользователя.
+     * @param birthday дата рождения пользователя.
+     */
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+    /**
+     * Метод возвращает имя пользователя.
+     * @return имя пользователя.
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * Метод возвращает количество детей пользователя.
+     * @return количество детей пользователя.
+     */
+    public int getChildren() {
+        return children;
+    }
+    /**
+     * Метод возвращает дата рождения пользователя.
+     * @return дата рождения пользователя.
+     */
+    public Calendar getBirthday() {
+        return birthday;
+    }
+}
