ada6c31e36ae68815f64f16bc6d5391603fc94cd
vovkalexander <vovk.ag747@gmail.com>
2019-07-24 14:18:20 +0300

    8. Реализовать собственную структуру данных - HashMap

---
+++ chapter_005/src/main/java/ru/job4j/map/SimpleHashMap.java
@@ -0,0 +1,172 @@
+package ru.job4j.map;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+/**
+ * SimpleHashMap.
+ * @author Vovk Alexander  vovk.ag747@gmail.com
+ * @version $Id$
+ * @since 0.1
+ */
+public class SimpleHashMap<K,V> implements Iterable<V>  {
+    /**
+     * Поле - хранит массив.
+     */
+    private Entry<K,V>[] table;
+    /**
+     * Поле - хранит размер массива.
+     */
+    private int size;
+    /**
+     * Поле - хранит идекс массива.
+     */
+    private int index;
+    /**
+     * Поле - хранит счетчик индексов.
+     */
+    private int count;
+    /**
+     * Поле - хранит счетчик изменение структуры массива.
+     */
+    private int modCount;
+    /**
+     * Поле - хранит степень заполнение массива.
+     */
+    private float loadFactor = 0.6f;
+    /**
+     * Конструктор для активации размера массива.
+     * @param size  размер массива.
+     */
+    public SimpleHashMap(int size) {
+        this.size = size;
+        this.table = new Entry[size];
+    }
+    /**
+     * Метод - добавляет объект в массив.
+     * @param key - ключ объекта.
+     * @param value - значение.
+     * @return логический вывод.
+     */
+    public boolean insert(K key, V value) {
+        if(loadFactor <= (float)count/size) {
+            increaseCapacity();
+        }
+        index = hash(key);
+        Entry<K,V> element = this.table[index];
+        if (element == null) {
+            this.table[index] = new Entry<>(key, value);
+            count++;
+            modCount++;
+            return true;
+        }
+        return  false;
+    }
+    /**
+     * Метод - рассчитывает индекс масива.
+     * @param key - ключ объекта.
+     * @return int - число.
+     */
+    private int hash(Object key) {
+        return  key.hashCode() % size;
+    }
+    /**
+     * Метод - увеличивает размер  массива при заполнении.
+     */
+    private void increaseCapacity() {
+        this.size = this.size * 2;
+        Entry<K, V>[] newTable = new Entry[this.size];
+        for (Entry<K, V> data : this.table) {
+            if (data != null) {
+                int i = hash((K) data.key);
+                newTable[i] = data;
+            }
+        }
+        this.table = newTable;
+    }
+    /**
+     * Метод - возвращает значение по ключу.
+     * @param key - ключ объекта.
+     * @return value - значение.
+     */
+    public V get(K key) {
+        int i = hash(key);
+        return (this.table[i] != null && key.equals(this.table[i].key) ? this.table[i].value : null);
+    }
+    /**
+     * Метод - удаляет объект по ключу.
+     * @param key - ключ объекта.
+     * @return логический ввывод.
+     */
+    public boolean delete(K key){
+        index = hash(key);
+        if(this.table[index] != null && key.equals(this.table[index].key)) {
+            this.table[index] = null;
+            count --;
+            return true;
+        }
+        return false;
+    }
+    /**
+     * Метод - возвращает размер массива.
+     * @return размер массива.
+     */
+    public int getSize() {
+        return size;
+    }
+    /**
+     * Метод - возвращает итератор .
+     */
+    @Override
+    public Iterator<V> iterator() {
+        return new Iterator<V>() {
+            private int position;
+            int expectedModCount = modCount;
+            @Override
+            public boolean hasNext() {
+                if (this.expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                boolean result = false;
+                while (position < table.length && table[position] != null) {
+                    position++;
+                }
+                return position < table.length;
+            }
+            @Override
+            public V next() {
+                while (position < table.length && table[position] == null) {
+                    position++;
+                }
+                if (position >= table.length) {
+                    throw new NoSuchElementException();
+                }
+                return (V) table[position++].value;
+            }
+        };
+    }
+    /**
+     * Entry<K,V> - внутренний класc
+     * @author Vovk Alexander  vovk.ag747@gmail.com
+     * @version $Id$
+     * @since 0.1
+     */
+    private class Entry<K,V> {
+        /**
+         * Поле - хранит массив.
+         */
+        K key;
+        /**
+         * Поле - хранит массив.
+         */
+        V value;
+        /**
+         * Конструктор для активации полей.
+         * @param key  ключ объекта.
+         * @param value - значение объекта.
+         */
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/map/SimpleHashMapTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.map;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+/**
+ * SimpleHashMapTest.
+ * @author Vovk Alexander  vovk.ag747@gmail.com
+ * @version $Id$
+ * @since 0.1
+ */
+public class SimpleHashMapTest {
+    SimpleHashMap<Integer, String> map;
+    @Before
+    public void setUp() {
+        map = new SimpleHashMap<>(10);
+    }
+    @Test
+    public void whenAddDifferentElementsThenReceiveValue() {
+        map.insert(23, "Jordan");
+        map.insert(7, "Ronaldo");
+        assertThat(map.get(7), is("Ronaldo"));
+    }
+    @Test
+    public void whenMapDeletesElementThenTrue() {
+        map.insert(23, "Jordan");
+        map.insert(7, "Ronaldo");
+        assertThat(map.delete(23), is(true));
+    }
+    @Test
+    public void whenMapIsAlmostFullThenDoubleSize() {
+        map.insert(23, "Jordan");
+        map.insert(33, "Pippen");
+        map.insert(10, "Pele");
+        map.insert(7, "Ronaldo");
+        map.insert(8, "Bryant");
+        map.insert(15, "Carter");
+        map.insert(21, "Dunkan");
+        map.insert(2, "Jonson");
+        assertThat(map.getSize(),is(20));
+    }
+    @Test(expected = NoSuchElementException.class)
+    public void whenIterateAfterReturningFinalValueNextThenExсeption() {
+        map.insert(23, "Jordan");
+        map.insert(2, "Jonson");
+        Iterator<String> it = map.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is("Jonson"));
+        assertThat(it.next(), is("Jordan"));
+        it.next();
+    }
+}
