dfb168dc44ee325922e0f5b021ec9b3253186ef9
vovkalexander <vovk.ag747@gmail.com>
2019-08-07 14:02:39 +0300

    1. Создать элементарную структуру дерева исправил

--- chapter_005/src/test/java/ru/job4j/tree/TreeTest.java
+++ chapter_005/src/test/java/ru/job4j/tree/TreeTest.java
@@ -1,8 +1,8 @@
 package ru.job4j.tree;
 import org.junit.Test;
+import java.util.Iterator;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-
 public class TreeTest {
     @Test
     public void when6ElFindLastThen6() {
@@ -34,9 +34,15 @@ public class TreeTest {
         tree.add(1, 4);
         tree.add(4, 5);
         tree.add(5, 6);
-        assertThat(tree.iterator().hasNext(), is(true));
-        assertThat(tree.iterator().next(), is(1));
-        assertThat(tree.iterator().hasNext(), is(true));
-        assertThat(tree.iterator().next(), is(2));
+        Iterator<Integer> it = tree.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(5));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(false));
     }
 }
