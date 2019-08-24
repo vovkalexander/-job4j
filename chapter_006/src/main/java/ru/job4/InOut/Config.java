edf8f73a54adeeb0152534bfea3a3d6eaebab1cf
vovkalexander <vovk.ag747@gmail.com>
2019-08-24 03:12:27 +0300

    1. Читаем файл конфигурации[#148441]

---
+++ chapter_006/src/main/java/ru/job4/InOut/Config.java
@@ -0,0 +1,81 @@
+package ru.job4.InOut;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+/**
+ * Config.
+ * @author Vovk Alexander  vovk.ag747@gmail.com
+ * @version $Id$
+ * @since 0.1
+ */
+public class Config {
+    /**
+     * Поле - хранит путь к файлу.
+     */
+    private final String path;
+    /**
+     * Поле - хранит хеш таблицу запесей в файле.
+     */
+    private final Map<String, String> values = new HashMap<String, String>();
+    /**
+     * Конструктор для активации пути к файлу.
+     * @param path  путь к файлу.
+     */
+    public Config(String path) {
+        this.path = path;
+    }
+    /**
+     * Метод - загружает файл в хеш таблицу.
+     */
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            String line = null;
+            while((line = read.readLine()) != null) {
+                String[] rst = line.split("=");
+                if (rst.length == 2) {
+                    for (int i = 0; i < rst.length; i+=2) {
+                        values.put(rst[i],rst[i+1]);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * Метод - возвращает значение по ключу.
+     * @param key - ключ хеш таблицы.
+     * @return  value - значение.
+     */
+    public String value(String key)  throws ValueNotFoundException {
+        String value = values.get(key);
+        if (value != null) {
+            return value;
+        }
+        throw new ValueNotFoundException("Don't impl this method yet!");
+    }
+    /**
+     * Метод - возвращает хеш таблицу.
+     * @return  Map - хеш таблица.
+     */
+    public Map<String,String> getMap() {
+        return this.values;
+    }
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+    public static void main(String[] args) {
+        Config conf = new Config("C:\\projects\\student\\Vovk Alexander\\-job4j\\app.properties");
+        System.out.println(conf);
+        conf.load();
+    }
+}
---
+++ chapter_006/src/test/java/ru/job4/InOut/ConfigTest.java
@@ -0,0 +1,35 @@
+package ru.job4.InOut;
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+/**
+ * ConfigTest
+ * @author Vovk Alexander (vovk.ag747@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ConfigTest {
+    Config config;
+    @Before
+    public void SetUp() {
+        config = new Config("C:\\projects\\student\\Vovk Alexander\\-job4j\\app.properties");
+    }
+    @Test
+    public void whenFileIsReadThenArrayHasSize() {
+        config.load();
+        assertThat(config.getMap().size(), is(5));
+    }
+    @Test
+    public void whenFileIsReadThenArrayCanGetValueByKey() throws ValueNotFoundException {
+        config.load();
+        assertThat(config.value("hibernate.connection.username"), is ("postgres"));
+        assertThat(config.value("hibernate.connection.password"), is ("password"));
+    }
+    @Test(expected = ValueNotFoundException.class)
+    public void whenKeyOfArrayNotEqualsValueThenException() throws ValueNotFoundException {
+        Config config = new Config("C:\\projects\\student\\Vovk Alexander\\-job4j\\app.properties");
+        config.load();
+        assertThat(config.value("password"), is ("password"));
+    }
+}
