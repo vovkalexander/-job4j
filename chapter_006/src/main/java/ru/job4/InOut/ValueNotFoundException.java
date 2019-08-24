efb04f2079526d7a967789098aa1449f79510d01
vovkalexander <vovk.ag747@gmail.com>
2019-08-24 03:16:15 +0300

    1. Читаем файл конфигурации[#148441]

---
+++ chapter_006/src/main/java/ru/job4/InOut/ValueNotFoundException.java
@@ -0,0 +1,16 @@
+package ru.job4.InOut;
+/**
+ * UnsupportedOperationException
+ * @author Vovk Alexander  vovk.ag747@gmail.com
+ * @version $Id$
+ * @since 0.1
+ */
+public class ValueNotFoundException extends Exception {
+    /**
+     * Метод реализаущий запрос параметров пользователя.
+     * @param msg - сообщение на консоль о наличие исключения.
+     */
+    public ValueNotFoundException(String msg) {
+        super(msg);
+    }
+}
