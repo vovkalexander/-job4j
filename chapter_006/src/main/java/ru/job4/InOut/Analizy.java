80137a28dcfea813aaa21653882fc41dd490ebc4
vovkalexander <vovk.ag747@gmail.com>
2019-09-02 02:23:25 +0300

    2. Анализ доступности сервера.

---
+++ chapter_006/src/main/java/ru/job4/InOut/Analizy.java
@@ -0,0 +1,60 @@
+package ru.job4.InOut;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Analizy.
+ * @author Vovk Alexander  vovk.ag747@gmail.com
+ * @version $Id$
+ * @since 0.1
+ */
+public class Analizy {
+    /**
+     * Метод - записывает в файл диапозон, когда сервер не работал.
+     * @param source - файл регистрации событий сервера.
+     * @param target - файл простоя сервера.
+     */
+    public void unavailable(String source,String target) {
+        try (BufferedReader read = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            boolean logic= true;
+            String value;
+            String line = null;
+            List<String> list = new ArrayList<>();
+            while ((line = read.readLine()) != null) {
+                if (logic) {
+                    if (line.contains("400") || line.contains("500")) {
+                        value = line.substring(4);
+                        list.add(value);
+                        logic = false;
+                    }
+                }
+                if (!logic) {
+                    if (line.contains("200") || line.contains("300")) {
+                        value = line.substring(4);
+                        list.add(value);
+                        logic= true;
+                    }
+                }
+            }
+            System.out.println(list);
+            for (int i = 0; i<list.size(); i += 2) {
+                System.out.println(list.size());
+                if(list.size() % 2 == 0) {
+                    out.write(String.join(";", list.get(i),list.get(i+1)));
+                    out.write(System.lineSeparator());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
+            out.println("15:01:30;15:02:32");
+            out.println("15:10:30;23:12:32");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ chapter_006/src/test/java/ru/job4/InOut/AnalizyTest.java
@@ -0,0 +1,22 @@
+package ru.job4.InOut;
+import org.junit.Test;
+import java.io.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+/**
+ * AnalizyTest
+ * @author Vovk Alexander (vovk.ag747@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class AnalizyTest {
+    @Test
+    public void when() throws IOException {
+        File file = new File("unavailable.csv");
+        Analizy analizy = new Analizy();
+        analizy.unavailable("\\C:\\projects\\student\\Vovk Alexander\\-job4j\\server", "unavailable.csv");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        assertThat(reader.readLine(), is("10:57:01;10:59:01"));
+        assertThat(reader.readLine(), is("11:01:02;11:02:02"));
+    }
+}
