55e3f77adb7ead99c98bd95e93919914f47bc9b9
vovkalexander <vovk.ag747@gmail.com>
2019-09-11 13:28:59 +0300

    3. Сканирование файловой системы исправил.

--- chapter_006/src/test/java/ru/job4/InOut/SearchTest.java
+++ chapter_006/src/test/java/ru/job4/InOut/SearchTest.java
@@ -1,7 +1,7 @@
 package ru.job4.InOut;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import static org.hamcrest.Matchers.is;
@@ -13,29 +13,25 @@ import static org.junit.Assert.assertThat;
  * @since 0.1
  */
 public class SearchTest {
+    File files = new File(System.getProperty("java.io.tmpdir"));
+    @Before
+    public void setUp() {
+        files.mkdirs();
+    }
     @Test
-    public void whenAskDifferentExtThenGetArrayOfExtFiles() throws IOException {
-        String way = System.getProperty("java.io.tmpdir") + File.separator + "test_search";
-        File test = new File(way);
-        test.mkdirs();
-        File file1 = new File(test, "a.pdf");
-        file1.createNewFile();
-        File file2 = new File(test, "b.pdf");
-        file2.createNewFile();
-        File folder = new File(test, "folder_search");
-        folder.mkdir();
-        File file3 = new File(folder, "c.pdf");
-        file3.createNewFile();
-        File file4 = new File(folder, "d.text");
-        file4.createNewFile();
-        List<String> ext1 = new ArrayList<>();
-        ext1.add("pdf");
-        List<String> ext2 = new ArrayList<>();
-        ext2.add("pdf");
-        ext2.add("text");
+    public void whenAskSingleExtThenGetArrayOfExtFiles() {
+        List<String> ext = new ArrayList<>();
+       ext.add("tmp");
+       Search search = new Search();
+       assertThat(search.files(files.getAbsolutePath(), ext).size(), is (18));
+    }
+    @Test
+    public void whenAskSomeExtThenGetArrayOfExtFiles() {
+        List<String> ext = new ArrayList<>();
+        ext.add("txt");
+        ext.add("log");
         Search search = new Search();
-        assertThat(search.files(test.getAbsolutePath(), ext1).size(), is(3));
-        assertThat(search.files(test.getAbsolutePath(), ext2).size(), is(4));
+        assertThat(search.files(files.getAbsolutePath(), ext).size(), is (22));
     }
 }
 
