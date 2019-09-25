4efec13aba942cce8255a10cf252ba9b79b02609
vovkalexander <vovk.ag747@gmail.com>
2019-09-26 02:39:33 +0300

    4. Архивировать проект. - исправил

--- chapter_006/src/test/java/ru/job4/InOut/ZipTest.java
+++ chapter_006/src/test/java/ru/job4/InOut/ZipTest.java
@@ -1,9 +1,9 @@
 package ru.job4.InOut;
 import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import java.io.File;
 import java.util.List;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
 /**
  * ZipTest.
  * @author Vovk Alexander (vovk.ag747@gmail.com)
@@ -13,12 +13,12 @@ import static org.hamcrest.Matchers.is;
 public class ZipTest {
     @Test
     public void archiveInZipTest() {
-        String[] args = {"-d", "C:\\projects\\student\\Vovk Alexander\\-job4j\\chapter_005", "-e", ".java", "-o", "C:\\projects\\student\\Vovk Alexander\\-job4j.zip"};
+        String[] args = {"-d", System.getProperty("user.dir"), "-e", ".java", "-o", "..\\test.zip"};
         Zip zip = new Zip(args);
         List<File> list = zip.seekBy(zip.arg.directory(), zip.arg.exclude());
         zip.pack(list, new File(zip.arg.output()));
-        File file = new File("C:\\projects\\student\\Vovk Alexander\\-job4j.zip");
-        assertThat(list.size(), is(68));
-        assertThat(file.length(), is(71540l));
+        File file = new File("..\\test.zip");
+        assertThat(list.size(), is(16));
+        assertThat(file.length(), is(17102l));
     }
 }
