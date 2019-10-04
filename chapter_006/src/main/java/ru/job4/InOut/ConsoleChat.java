c2e9516e38182368ff00e54592370449feedbaf9
vovkalexander <vovk.ag747@gmail.com>
2019-10-04 13:50:32 +0300

    5. Создать программу 'Консольный чат'.

---
+++ chapter_006/src/main/java/ru/job4/InOut/ConsoleChat.java
@@ -0,0 +1,113 @@
+package ru.job4.InOut;
+import java.io.*;
+import java.util.*;
+/**
+ * ConsoleChat.
+ * @author Vovk Alexander  vovk.ag747@gmail.com
+ * @version $Id$
+ * @since 0.1
+ */
+public class ConsoleChat {
+    /**
+     * Поле - поток консольного ввода.
+     */
+    private Scanner in;
+    /**
+     * Поле - хранит список переписки.
+     */
+    private List<String> link = new LinkedList<>();
+    /**
+     * Поле - хранит путь к файлу записи диалога.
+     */
+    private String parent= System.getProperty("user.dir") + File.separator + "test.txt";
+    /**
+     * Конструктор  для активации консольного потока
+     * @param stream поток ввода.
+     */
+    public ConsoleChat(InputStream stream) {
+        this.in = new Scanner(stream);
+
+    }
+    /**
+     * Конструктор  для активации консольного потока
+     * @param input  строки пользователя.
+     */
+    public ConsoleChat(String input ) {
+        this.in = new Scanner(input);
+
+    }
+    /**
+     * Метод возвращает файл с исключающем расширением .
+     * @return список переписки.
+     */
+    public List<String> getList() {
+        return this.link;
+    }
+    /**
+     * Метод загружает список выражений ответчика.
+     * @param logic - выключатель потока ответчика.
+     * @return список выражений ответчика.
+     */
+    public List<String> load(Boolean logic) throws IOException {
+        List<String> list = new ArrayList<>();
+        Boolean rst = true;
+        try (BufferedReader read = new BufferedReader(new FileReader(this.parent))) {
+            if (rst == logic) {
+                String string;
+                while ((string = read.readLine()) != null) {
+                    list.add(string);
+                }
+            } else {
+                read.close();
+                list.clear();
+            }
+        }
+        return list;
+    }
+    /**
+     * Метод записывает переписку в файл.
+     * @param list - список переписки.
+     * @param  path - путь к файлу
+     */
+    public void writeDown(List<String> list, String path) throws IOException {
+        try (BufferedWriter out = new BufferedWriter(new FileWriter(path))) {
+            for (String string : list) {
+                out.write(string + System.lineSeparator());
+            }
+        }
+
+    }
+    /**
+     * Метод создает переписку.
+     */
+    public void setUp() throws IOException {
+        Boolean check = true;
+        String scan;
+        String answer;
+        while (in.hasNext() && !(scan = in.next()).equals("закончить")) {
+            link.add(scan);
+            if (scan.equals("стоп")) {
+                check = false;
+            }
+            if (scan.equals("продолжить")) {
+                check = true;
+            }
+            List<String> array = load(check);
+            Collections.shuffle(array);
+            for (int i = 0; i < array.size(); i++) {
+                answer = array.get(i);
+                System.out.println(answer);
+                link.add(answer);
+                break;
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        ConsoleChat chat = new ConsoleChat(System.in);
+        chat.setUp();
+        chat.writeDown(chat.getList(), System.getProperty("user.dir") + File.separator + "result.txt");
+    }
+}
+
+
+
---
+++ chapter_006/src/test/java/ru/job4/InOut/ConsoleChatTest.java
@@ -0,0 +1,47 @@
+package ru.job4.InOut;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * ConsoleChatTest.
+ * @author Vovk Alexander (vovk.ag747@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ConsoleChatTest {
+    String str = System.getProperty("user.dir")+ File.separator + "result";
+    @Test
+    public void whenFinishAtOnceThenEmptyArray() throws IOException {
+        ConsoleChat chat = new ConsoleChat("закончить");
+        chat.setUp();
+        assertThat(chat.getList().size(), is (0));
+    }
+    @Test
+    public void whenChatHasTalkThenFieldOfChatEqualsFile() throws IOException {
+        ConsoleChat chat = new ConsoleChat("Привет"+ "\n" +
+                "какдела" +"\n" +
+                "что?" + "\n" +
+                "стоп" +"\n"+
+                "помолчим" +"\n"+
+                "уговорил" + "\n" +
+                "продолжить" + "\n" +
+                "закончить");
+        chat.setUp();
+        chat.writeDown(chat.getList(), str);
+        List<String> expect = new ArrayList<>();
+        String string;
+        try (BufferedReader read = new BufferedReader(new FileReader(str))) {
+            while ((string = read.readLine()) != null) {
+                expect.add(string);
+            }
+        }
+        assertThat(chat.getList().size(), is (11));
+        assertThat(chat.getList().size(), is (expect.size()));
+    }
+}
