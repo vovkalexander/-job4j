f4b7cd9ee55e1e4c17ba3f5aafc7f98792e912db
Александр <46612532+vovkalexander@users.noreply.github.com>
2020-01-07 03:21:48 +0300

    Create SQL Select by company.sql
    SQL Select by company

---
+++ chapter_007/src/main/java/ru/job4j/sql/SQL Select by company.sql
@@ -0,0 +1,19 @@
+﻿--1) Retrieve in a single query:
+--- names of all persons that are NOT in the company with id = 5
+--- company name for each person
+Select p.name, c.name  from company as c
+LEFT OUTER JOIN person as p
+ON p.company_id = c.id
+AND p.company_id not in(5);
+
+--2) Select the name of the company with the maximum number of persons + number of persons in this company
+SELECT c.name, COUNT(*) AS quant FROM company AS c
+INNER JOIN person AS p 
+ON p.company_id=c.id
+GROUP BY c.name
+ORDER BY quant desc
+LIMIT 1;
+
+
+ 
+
