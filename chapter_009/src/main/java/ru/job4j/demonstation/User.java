7122dd147713d050ff0bb59a488e6cf1ff526250
Александр <46612532+vovkalexander@users.noreply.github.com>
2020-05-30 00:50:49 +0300

    Демонстрация работы GC

---
+++ chapter_009/src/main/java/ru/job4j/demonstation/User.java
@@ -0,0 +1,74 @@
+package ru.job4j.demonstation;
+/**
+ * User.
+ * @author Vovk Alexander  vovk.ag747@gmail.com
+ * @version $Id$
+ * @since 0.1
+ */
+public class User {
+    /**
+     * Field - stores int value.
+     */
+    private int id;
+    /**
+     * Field - stores link for object of  String.
+     */
+    private String name;
+    /**
+     * Field - stores link for object of  String.
+     */
+    private String city;
+    /**
+     * Empty constructor.
+     */
+    public User() {
+    }
+    /**
+     * Overloaded Constructor for activation 2 fields.
+     */
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    /**
+     * Overloaded Constructor for activation 3 fields.
+     */
+    public User(int id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("Object has been destroyed");
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+    /**
+     * Method makes big quality Object of class.
+     * @param limit - primitive variable.
+     */
+    public void method(int limit) {
+        info();
+        for(int index = 0; index < limit; index++) {
+            new User(1,"Pupkin", "City");
+        }
+        info();
+    }
+    /**
+     * Method displays information about heap.
+     */
+    public  void info() {
+        int mb = 1024 * 1024;
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println("Heap performs statistics [MB] ");
+        System.out.println("Used Memory:" + (runtime.totalMemory() - runtime.freeMemory()) / mb);
+        System.out.println("Total Memory:" + runtime.totalMemory() / mb);
+        System.out.println("Maximum Memory:" + runtime.maxMemory() / mb);
+    }
+}
---
+++ chapter_009/src/test/java/ru/job4j/demonstration/UserTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.demonstration;
+import ru.job4j.demonstation.EmptyUser;
+import ru.job4j.demonstation.User;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static com.carrotsearch.sizeof.RamUsageEstimator.sizeOf;
+/**
+ * UserTest
+ * @author Vovk Alexander (vovk.ag747@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class UserTest {
+    @Test
+    public void memoryTestOfEmptyObjectWithoutFields() {
+        EmptyUser user = new EmptyUser();
+        assertThat(sizeOf(user), is(16L));
+    }
+    @Test
+    public void memoryTest() {
+        User user1 = new User();
+        User user2 = new User(1, "Carlson");
+        User user3 = new User(1,"Winnie the Pooh", "London");
+        assertThat(sizeOf(user1), is (24L));
+        assertThat(sizeOf(user2), is (72L));
+        assertThat(sizeOf(user3), is (128L));
+    }
+    @Test
+    public void whenGCDeleteObjectByItself() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        User user = new User();
+        System.setOut(new PrintStream(out));
+        user.method(30000);
+        assertThat(out.toString().contains("Object has been destroyed"), is (true));
+    }
+    @Test(expected = OutOfMemoryError.class)
+    public void whenHeapIsFull() throws OutOfMemoryError {
+        User user = new User();
+        user.method(3000000);
+    }
+
+}
