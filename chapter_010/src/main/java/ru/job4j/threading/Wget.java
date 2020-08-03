fa41f93b5c8fb47ddb348be01b94f0abf2061b15
Александр <46612532+vovkalexander@users.noreply.github.com>
2020-08-04 00:25:49 +0300

    3.1. Прерывание блокированной нити.

--- chapter_010/src/main/java/ru/job4j/threading/Wget.java
+++ chapter_010/src/main/java/ru/job4j/threading/Wget.java
@@ -15,7 +15,7 @@ public class Wget {
                             System.out.print("\rLoading : " + index  + "%");
                         }
                     } catch (InterruptedException e) {
-                        e.printStackTrace();
+                        Thread.currentThread().interrupt();
                     }
                 }
         );
