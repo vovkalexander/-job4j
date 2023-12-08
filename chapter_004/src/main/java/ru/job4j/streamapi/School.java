0592a9bedcdfcda6a899abe6904acb0927e2f90a
vovkalexander <vovk.ag747@gmail.com>
2023-12-08 16:25:23 +0300

    fix checkstyle

--- chapter_004/src/main/java/ru/job4j/streamapi/School.java
+++ chapter_004/src/main/java/ru/job4j/streamapi/School.java
@@ -7,7 +7,6 @@ import java.util.stream.Collectors;
 
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predicate) {
-
         return students.stream()
                 .filter(predicate)
                 .collect(Collectors.toList());
