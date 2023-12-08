38b34722284aa714afb1ffeda2a8b274a20aa8f1
vovkalexander <vovk.ag747@gmail.com>
2023-12-08 16:31:41 +0300

    fix checkstyle

--- chapter_004/src/main/java/ru/job4j/streamapi/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/streamapi/Profiles.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
-
         return profiles
                 .stream()
                 .map(Profile::getAddress)
